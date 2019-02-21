254e50929d5442a76f74847abafe2fa2f784ad59
Remoll <remigiusz.slusarczyk@gmail.com>
2019-02-21 13:04:13 +0100

    optimization of code.js

--- code.js
+++ code.js
@@ -1,28 +1,36 @@
-const menuTab = [];
-menuTab.length = 4;
-for (let i = 0; i < menuTab.length; i++) {
-    menuTab[i] = document.querySelector("li:nth-child(" + (i + 1) + ")");
-}
 const ball = document.querySelector("nav");
 const burger = document.querySelector(".hamburger");
+
 const divs = [];
 divs.length = 3;
 for (let i = 0; i < divs.length; i++) {
-    divs[i] = document.querySelector(".hamburger div:nth-child(" + (i + 1) + ")");
+    divs[i] = document.querySelector(`.hamburger div:nth-child(${i + 1})`);
 }
+
+const menuTab = [];
+menuTab.length = 4;
+for (let i = 0; i < menuTab.length; i++) {
+    menuTab[i] = document.querySelector(`li:nth-child(${i + 1})`);
+}
+
 burger.addEventListener('click', function () {
     ball.classList.toggle("hidden");
+    // menuTab.forEach(classList.remove("postAnimation"));        do dopracowania i dodanie pozniej
     for (let i = 0; i < menuTab.length; i++) {
         menuTab[i].classList.remove("postAnimation");
     };
-    for (let j = 0; j < 3; j++) {
+    // divs.forEach(classList.toggle("clicked"));                   do dopracowania i dodanie pozniej
+    for (let j = 0; j < divs.length; j++) {
         divs[j].classList.toggle("clicked");
     };
 });
+// menuTab.forEach(addEventListener('mouseover', function () {      do dopracowania i dodanie pozniej
+//     menuTab.forEach(classList.add("postAnimation"));
+// }));
 for (let i = 0; i < menuTab.length; i++) {
     menuTab[i].addEventListener('mouseover', function () {
-        for (let j = 0; j < 4; j++) {
+        for (let j = 0; j < menuTab.length; j++) {
             menuTab[j].classList.add("postAnimation");
         }
     });
-};
+}
