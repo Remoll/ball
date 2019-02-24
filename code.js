4097280347606bd6ec87f280eed8309d33f66747
Remoll <remigiusz.slusarczyk@gmail.com>
2019-02-24 19:50:53 +0100

    clear code.js

--- code.js
+++ code.js
@@ -1,36 +1,12 @@
 const ball = document.querySelector("nav");
 const burger = document.querySelector(".hamburger");
-
-const divs = [];
-divs.length = 3;
-for (let i = 0; i < divs.length; i++) {
-    divs[i] = document.querySelector(`.hamburger div:nth-child(${i + 1})`);
-}
-
-const menuTab = [];
-menuTab.length = 4;
-for (let i = 0; i < menuTab.length; i++) {
-    menuTab[i] = document.querySelector(`li:nth-child(${i + 1})`);
-}
-
+const divs = document.querySelectorAll(".hamburger div");
+const menuTab = document.querySelectorAll("li");
 burger.addEventListener('click', function () {
     ball.classList.toggle("hidden");
-    // menuTab.forEach(classList.remove("postAnimation"));        do dopracowania i dodanie pozniej
-    for (let i = 0; i < menuTab.length; i++) {
-        menuTab[i].classList.remove("postAnimation");
-    };
-    // divs.forEach(classList.toggle("clicked"));                   do dopracowania i dodanie pozniej
-    for (let j = 0; j < divs.length; j++) {
-        divs[j].classList.toggle("clicked");
-    };
+    menuTab.forEach((menuTabItem) => menuTabItem.classList.remove("postAnimation"));
+    divs.forEach((div) => div.classList.toggle("clicked"));
 });
-// menuTab.forEach(addEventListener('mouseover', function () {      do dopracowania i dodanie pozniej
-//     menuTab.forEach(classList.add("postAnimation"));
-// }));
-for (let i = 0; i < menuTab.length; i++) {
-    menuTab[i].addEventListener('mouseover', function () {
-        for (let j = 0; j < menuTab.length; j++) {
-            menuTab[j].classList.add("postAnimation");
-        }
-    });
-}
+menuTab.forEach((menuTabItem) => {
+    menuTabItem.addEventListener("mouseover", () => menuTab.forEach((menuTabItem) => menuTabItem.classList.add("postAnimation")))
+});
